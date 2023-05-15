cbbdbbd6fd2b150698897ce6fcb6e6205b388350
wezer-pixel <jerryjuma104@gmail.com>
2023-05-15 02:48:54 -0400

    Betty would be proud

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * sub - subtract two numbers
+ * @num1: number to be subtracted from the second number
+ * @num2: Number that is subtracted
+ *
+ * Return: Difference of the two numbers
+ */
+
+int sub(num1, num2)
+{
+	return(num2 - num1);
+}
